240415a750559dd7d56464521eb03e98717675a3
Paulespinozarl <paulespinozarl@gmail.com>
2023-05-26 16:23:24 -0300

    Solucion de error en declaracion de variable

--- controllers/events.js
+++ controllers/events.js
@@ -41,24 +41,19 @@ const crearEvento = async (req, res = response) => {
 const actualizarEvento = async (req, res = response) => {
 
     const eventoId = req.params.id;
-    const uid = req.params.id;
+    const uid = req.uid;
 
     try {
         
         const evento = await Evento.findById( eventoId );
+
         if ( !evento ) {
             return res.status(404).json({
                 ok: false,
                 msg: 'Evento no existe por ese id'
             });
         }
-
-        console.log({
-            userEventDB : evento?.user?.toString() || '',  
-            uid : uid || '',
-            isUserValidToEdit : evento?.user?.toString() !== uid,
-          });
-
+        
         if ( evento.user.toString() !== uid ) {
             return res.status(401).json({
                 ok: false,
@@ -91,7 +86,7 @@ const actualizarEvento = async (req, res = response) => {
 
 const eliminarEvento = async (req, res = response) => {
     const eventoId = req.params.id;
-    const uid = req.params.id;
+    const uid = req.uid;
 
     try {
         
